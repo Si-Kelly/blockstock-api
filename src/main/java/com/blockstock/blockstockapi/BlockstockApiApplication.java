f9265dada7892bfcba3c0d6c13bb4ae7fd266f33
Simon Kelly <si@aracelis.co.uk>
2022-10-25 08:16:45 +0100

    first commit

---
+++ src/main/java/com/blockstock/blockstockapi/BlockstockApiApplication.java
@@ -0,0 +1,13 @@
+package com.blockstock.blockstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockstockApiApplication.class, args);
+	}
+
+}
